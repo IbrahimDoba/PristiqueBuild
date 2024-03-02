371132e4efea821d61df2ec57eeeffbfeb22979e
Ibrahim doba <ibrahimdoba55@gmail.com>
2024-03-03 00:55:51 +0100

    UI: vide bg chamges

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,22 @@
-import Designs from '@/components/DesignSection/SectionsPage';
-import Homepage from '@/components/HeroSection/hero';
-import SliderPage from '@/components/SliderSection/SliderPage';
+import Designs from "@/components/DesignSection/SectionsPage";
+import Homepage from "@/components/HeroSection/hero";
+import SliderPage from "@/components/SliderSection/SliderPage";
+import VideoBgSection from "@/components/videoBgSection/videoBgSection";
 
 export default function Home() {
   return (
     <main>
       <div className=" bg-bg  flex flex-col justify-center items-center">
-      {/* <Navbar /> */}
-      <Homepage/>
-      {/* our core values
+        {/* <Navbar /> */}
+        <VideoBgSection/>
+        <Homepage />
+        {/* our core values
       futurestic designs (vr) */}
-      <SliderPage/>       {/* // our services */}
-
-      <Designs/>       {/* // our process */}
-
-      {/* <AboutUsSection /> */}
-      {/* // contact */}
-      {/* <Footer/> */}
+        <SliderPage /> {/* // our services */}
+        <Designs /> {/* // our process */}
+        {/* <AboutUsSection /> */}
+        {/* // contact */}
+        {/* <Footer/> */}
       </div>
     </main>
   );
--- client/src/components/videoBgSection/videoBgSection.tsx
+++ client/src/components/videoBgSection/videoBgSection.tsx
@@ -1,18 +1,34 @@
+"use client";
 import React from "react";
-// import modularVid from ""
+import { motion } from "framer-motion";
 
-const VideoBgSection = () => {
+const VideoBgSection = ({ isVisible }: any) => {
   return (
-    <header className="relative  flex items-center justify-center h-screen mb-12 overflow-hidden w-full">
-      <div className="relative w-[40%] z-30 p-5 text-2xl text-txt  bg-opacity-50 rounded-xl  right-[20%] top-[15%]">
+    <header className="relative  flex items-center justify-center h-screen  overflow-hidden w-full">
+      <div className="relative w-[550px] z-30 p-5 text-2xl text-txt  bg-opacity-50 rounded-xl  right-[10%] bottom-[20%]">
         <p className="text-amber-800 font-medium text-2xl">Our Story</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-          perspiciatis.
-        </p>
+        <motion.div
+          // whileHover={{ scale: 1.2 }}
+          // whileTap={{ scale: 1.1 }}
+          // drag="x"
+          // dragConstraints={{ left: -100, right: 100 }}
+        >
+          <p>
+            Welcome to Prestique Build, where innovation meets construction
+            excellence. At Prestique Build, were not just building structures;
+            were shaping the future of construction through our commitment to
+            modular excellence.
+          </p>
+        </motion.div>
       </div>
-      <div>
-
+      <div className="relative w-[550px] z-30 p-5 text-2xl text-txt  bg-opacity-50 rounded-xl left-[10%] top-[10%] ">
+        <p>
+          Founded with a vision to revolutionize the construction industry,
+          Prestique Build stands at the forefront of cutting-edge modular
+          construction solutions. With years of expertise and a passion for
+          pushing boundaries, we have earned a reputation for delivering
+          high-quality, sustainable, and cost-effective modular projects.
+        </p>
       </div>
       <video
         autoPlay
